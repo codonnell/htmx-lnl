cdd5aadf6c00dad12231974b4271fa8f65eec2f3
Chris O'Donnell <chris.bowdoin@gmail.com>
2024-01-09 20:36:46 -0500

    feat: enable hx-boost

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODOs
+* [x] Enable hx-boost
+* [ ] Implement client-side title validation
+* [ ] Implement inline delete
+* [ ] Implement drag and drop
---
+++ src/htmx_lnl/view.clj
@@ -0,0 +1,80 @@
+(ns htmx-lnl.view
+  (:require
+   [hiccup2.core :as h]
+   [htmx-lnl.db :as db]))
+
+(defn page-layout [content]
+  (str
+   (h/html
+    {:mode :html}
+    (h/raw "<!DOCTYPE html>")
+    [:html
+     [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport"
+              :content "width=device-width, initial-scale=1"}]
+      [:title "Hello World"]
+      [:link {:rel "stylesheet"
+              :href "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"}]
+      [:script {:src "https://kit.fontawesome.com/9f53493378.js"
+                :crossorigin "anonymous"}]
+      [:script {:src "https://unpkg.com/htmx.org@1.9.10"
+                :crossorigin "anonymous"}]]
+     [:body {:hx-boost "true"}
+      [:section.section
+       content]]])))
+
+(defn kanban-card [card]
+  [:div.card
+   [:header.card-header
+    [:p.card-header-title (:title card)]
+    [:button.card-header-icon
+     [:a {:href (str "/" (:id card) "/edit")}
+      [:span.icon
+       [:i.fa-solid.fa-pencil]]]]]])
+
+(defn kanban-column [{:keys [stage cards]}]
+  (into [:div.column [:h2.subtitle.has-text-centered (get db/stages stage)]] (map kanban-card) cards))
+
+(defn trial-board [cards-by-stage]
+  [:div
+   [:a.button.is-link.is-info {:href "/create"} "Create new card"]
+   (into [:div.columns.mt-3]
+         (map (fn [stage] (kanban-column {:stage stage :cards (get cards-by-stage stage)})))
+         (keys db/stages))])
+
+(defn edit-card-form
+  ([]
+   (edit-card-form {} {}))
+  ([card]
+   (edit-card-form card {}))
+  ([card errors]
+   [:div
+    [:form#edit-form {:method "post"}]
+    (when (:id card)
+      [:form#delete-form {:method "post" :action (str "/" (:id card) "/delete")}])
+
+    [:div.field
+     [:label.label "Title"]
+     [:div.control
+      [:input.input {:form "edit-form" :type "text" :name "title" :placeholder "Card title" :value (:title card "")}]]
+     [:p.help.is-danger (get errors :title)]]
+    [:div.field
+     [:label.label "Stage"]
+     [:div.control
+      [:div.select
+       (into [:select {:name "stage" :form "edit-form"}]
+             (map (fn [[k v]]
+                    [:option (cond-> {:value k}
+                               (= k (:stage card))
+                               (assoc :selected true)) v]))
+             db/stages)]]
+     [:p.help.is-danger (get errors :stage)]]
+    [:div.field.is-grouped
+     [:div.control
+      [:button.button.is-link {:form "edit-form"} "Submit"]]
+     [:div.control
+      [:a.button.is-link.is-light {:href "/"} "Cancel"]]
+     (when (:id card)
+       [:div.control
+        [:button.button.is-danger {:form "delete-form"} "Delete"]])]]))
